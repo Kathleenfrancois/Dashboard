615e296163642feb5a0ae5a31ab9bd2c1ccb4ff2
Kathleenfrancois <106890313+Kathleenfrancois@users.noreply.github.com>
2022-08-10 12:28:44 -0400

    k

--- app/models.py
+++ app/models.py
@@ -1,10 +1,9 @@
 from sqlalchemy import Column, Float, String, Integer, Numeric
 from .database import Base
-
 class salary(Base):
     __tablename__ = "salary"
-    id = Column(Integer, primary_key=True,index=True)
-    player= Column(String, unique=True, index=True)
-    positions = Column(String)
+    id = Column(Integer, primary_key=True, index=True)
+    player = Column(String, unique=True, index=True)
+    fieldposition = Column(String)
     team = Column(String)
     salary= Column(Numeric)
