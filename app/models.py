81588fccc56a28509a3b520e0efa04bfd1a4bbc0
Kathleenfrancois <106890313+Kathleenfrancois@users.noreply.github.com>
2022-08-10 14:40:16 -0400

    k

--- app/models.py
+++ app/models.py
@@ -4,6 +4,6 @@ class salary(Base):
     __tablename__ = "salary"
     id = Column(Integer, primary_key=True, index=True)
     player = Column(String, unique=True, index=True)
-    fieldposition = Column(String)
+    positions = Column(String)
     team = Column(String)
     salary= Column(Numeric)
