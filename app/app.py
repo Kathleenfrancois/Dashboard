1130ae004836684b115acad18f9fdd20a730d93a
Kathleenfrancois <106890313+Kathleenfrancois@users.noreply.github.com>
2022-08-16 01:56:42 -0400

    Update app.py

--- app/app.py
+++ app/app.py
@@ -24,27 +24,6 @@ def get_db():
     finally:
         db.close()
 
-
-from fastapi import FastAPI, Depends, Request
-from sqlalchemy.orm import Session
-from . import crud, models
-from .database import SessionLocal, engine
-from fastapi.responses import HTMLResponse
-from fastapi.staticfiles import StaticFiles
-from fastapi.templating import Jinja2Templates
-import plotly.express as px
-import pandas as pd
-models.Base.metadata.create_all(bind=engine)
-app = FastAPI()
-app.mount("/static", StaticFiles(directory="static"), name="static")
-templates = Jinja2Templates(directory="templates")
-def get_db():
-    db = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
-
 @app.get("/")
 async def welcome(request: Request, db: Session=Depends(get_db)):
     x=crud.get_salary(db)
