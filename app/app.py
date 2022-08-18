e2afe0ea98a130a465a4af754b451f64be46bcfe
Kathleenfrancois <106890313+Kathleenfrancois@users.noreply.github.com>
2022-08-18 16:21:08 -0400

    Update app.py

--- app/app.py
+++ app/app.py
@@ -27,16 +27,18 @@ def get_db():
 @app.get("/")
 async def welcome(request: Request, db: Session=Depends(get_db)):
     x=crud.get_salary(db)
-    df = pd.DataFrame.from_records(x,columns=['Player','Positions','Team','Salary'])
-
+    df =
+pd.DataFrame.from_records(x,columns=['Player','Positions','Team','Salary'])
     px. defaults.width = 266
     px. defaults.height = 200
 
+    fig = px.bar(df.head(10),x='Player', y='Salary',
+color='Positions').update_xaxes(categoryorder="total descending")
     fig = px.bar(df.head(10),x='Player', y='Salary',title='Top 10 Paid NFL Players')
     fig.update_layout( yaxis = dict( tickfont = dict(size=5)),
-    xaxis = dict( tickfont = dict(size=5)),
-    font=dict(size=5),
-    margin=dict(l=0, r=0, t=0, b=0))
+        xaxis = dict( tickfont = dict(size=5)),
+        font=dict(size=5),
+        margin=dict(l=0, r=0, t=0, b=0))
     top10=fig.to_html(full_html=False, include_plotlyjs='cdn')
 
     dfteam = df.groupby('Team')['Salary'].sum()
@@ -45,9 +47,57 @@ async def welcome(request: Request, db: Session=Depends(get_db)):
 
     fig10 = px.bar(dfteam, x='Team', y='Salary')
     fig10.update_layout( yaxis = dict( tickfont = dict(size=5)),
-    xaxis = dict( tickfont = dict(size=5)),
-    font=dict(size=5),
-    margin=dict(l=0, r=0, t=0, b=0))
+        xaxis = dict( tickfont = dict(size=5)),
+        font=dict(size=5),
+        margin=dict(l=0, r=0, t=0, b=0))
     team10 = fig10.to_html(full_html=False, include_plotlyjs='cdn')
 
-    return templates.TemplateResponse("chart.html", {"request": request, "top10":top10, "team10":team10})
+    dfteam = df.loc[df['Team'].isin(dfteam.Team)]
+    figteam = px.bar(dfteam, x='Team', y='Salary',
+color='PositionS').update_xaxes(categoryorder="total descending")
+    figteam.update_layout(yaxis = dict(tickfont = dict(size=5)),
+        xaxis = dict(tickfont = dict(size=5)),
+        font=dict(size=5),
+        margin=dict(l=0, r=0, t=0, b=0))
+    teamsalary = figteam.to_html(full_html=False, include_plotlyjs='cdn')
+    pos10 = dfteam.groupby('Positions')
+['Salary'].mean().sort_values(ascending=False).head(10)
+    pos10 = pos10.reset_index()
+    figpos = px.box(dfteam.loc[dfteam['Positions'].isin(pos10.Positions)],
+x='Positions', y='Salary')
+    figpos.update_layout(yaxis = dict(tickfont = dict(size=5)),
+        xaxis = dict(tickfont = dict(size=5)),
+        font=dict(size=5),
+        margin=dict(l=0, r=0, t=0, b=0))
+    possalary = figpos.to_html(full_html=False, include_plotlyjs='cdn')
+
+    bottom10 = df.groupby('Team')['Salary'].sum()
+    bottom10 = bottom10.reset_index()
+    bottom10 = dfteam.sort_values('Salary', ascending=False).tail(10)
+    dfteam = df.loc[df['Team'].isin(bottom10.Team)]
+
+    pos10 = dfteam.groupby('Positions')
+['Salary'].mean().sort_values(ascending=False).head(10)
+    pos10 = pos10.reset_index()
+    figpos2 = px.box(dfteam.loc[dfteam['Positions'].isin(pos10.Positions)],
+x='Positions', y='Salary', color_discrete_sequence=['red'])
+    figpos2.update_layout(yaxis = dict(tickfont = dict(size=5)),
+        xaxis = dict(tickfont = dict(size=5)),
+        font=dict(size=5),
+        margin=dict(l=0, r=0, t=0, b=0))
+    possalary2 = figpos2.to_html(full_html=False, include_plotlyjs='cdn')
+
+    dfteam = df.groupby('Positions')['Salary'].mean()
+    dfteam = dfteam.reset_index()
+    dfteam = dfteam.sort_values('Salary', ascending=False)
+
+    figpie = px.pie(dfteam, values='Salary', names='Positions')
+    figpie.update_layout(yaxis = dict(tickfont = dict(size=5)),
+        xaxis = dict(tickfont = dict(size=5)),
+        font=dict(size=5),
+        margin=dict(l=0, r=0, t=0, b=0))
+    pospie = figpie.to_html(full_html=False, include_plotlyjs='cdn')
+
+    return templates.TemplateResponse("chart.html", {"request": request,
+"top10":top10, "team10":team10,
+"teamsalary":teamsalary,"possalary":possalary,"possalary2":possalary2,"pospie":pospie})
